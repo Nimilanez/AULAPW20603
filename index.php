4b3b7058aa17fea9fa2fedc85b34ef431f5a0476
Nii 3 <100323103+Nimilanez@users.noreply.github.com>
2023-03-06 15:57:54 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+<title>Exemplo de Formulario</title>
+</head>
+ <body>
+   <form method="POST" action="teste.class.php">
+       Nome: <input type="text"  name="nome" id="nome"><br><br>
+       <input type="submit" name="Cadrastar" value="Cadrastar">
+</form>
+</body>
+</html>
+ 
+    
